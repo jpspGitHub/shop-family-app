71c13f5b310879ad279179eaeaf1685abea3f824
Pablo Silva <pablosilva@192.168.1.10>
2025-06-06 20:38:53 -0300

    Add ios app and remove analisis files

---
+++ ios/shop-family-app/shop-family-app/shop_family_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  shop_family_appApp.swift
+//  shop-family-app
+//
+//  Created by Pablo Silva on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct shop_family_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
