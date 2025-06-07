77036632d2bfcc6877776ede1b3f131a882244f5
Pablo Silva <josepablo.silvaperez@gmail.com>
2025-06-06 21:34:04 -0300

    #44 redesign home screen

--- android/app/src/main/java/com/thaya/shop_family/ui/MainActivity.kt
+++ android/app/src/main/java/com/thaya/shop_family/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.thaya.shop_family.R
+import com.thaya.shop_family.utils.SessionManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navHost = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        setupActionBarWithNavController(navHost.navController)
+        val navController = navHost.navController
+
+        val sessionManager = SessionManager(this)
+        val graph = navController.navInflater.inflate(R.navigation.nav_graph)
+        if (sessionManager.fetchAuthToken() != null) {
+            graph.setStartDestination(R.id.homeFragment)
+        } else {
+            graph.setStartDestination(R.id.loginFragment)
+        }
+        navController.graph = graph
+
+        setupActionBarWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
